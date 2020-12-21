50f0668f834acc576e81ae2822a6d61f8605393f
AlbertUnruh <73029826+AlbertUnruh@users.noreply.github.com>
2020-12-21 16:04:15 +0100

    Update __init__.py
    adds now only commands, which satisfies the requirements

--- Modules/__init__.py
+++ Modules/__init__.py
@@ -2,11 +2,20 @@ from os import listdir
 import importlib
 
 
+required = ["__main__", "Branch", "HELP"]
+
 Modules = [_.replace(".py", "") for _ in listdir("./Modules") if _.endswith(".py") and not _ == "__init__.py"]
 libs = {}
 
 
-for lib in Modules:
+for lib in Modules[:]:
     libs[lib] = importlib.import_module(f"Modules.{lib}", "Modules")
 
-del listdir, lib, importlib
+    for attr in required:
+        if not hasattr(libs[lib], attr):
+            del libs[lib]
+            Modules.remove(lib)
+            continue
+
+
+del listdir, lib, importlib, attr
