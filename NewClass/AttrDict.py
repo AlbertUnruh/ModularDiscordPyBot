b964da077dcf1e685c22d7b5150c816da2b46bc7
AlbertUnruh <73029826+AlbertUnruh@users.noreply.github.com>
2020-12-22 20:35:24 +0100

    Add files via upload

---
+++ NewClass/AttrDict.py
@@ -0,0 +1,31 @@
+
+class AttrDict:
+    def __init__(self, data: dict):
+        for key in list(data.keys()):
+            if type(data[key]) == dict:
+                self.__dict__[key.replace(" ", "_")] = self.__class__(data[key])
+            else:
+                self.__dict__[key.replace(" ", "_")] = data[key]
+
+    def __str__(self):
+        def recursive(data: AttrDict):
+            __ = ""
+            for key in list(data.__dict__.keys()):
+                if type(data[key]) == self.__class__:
+                    __ += f"{key!r}: " "{" + recursive(data[key]) + "}, "
+                else:
+                    __ += f"{key!r}: {data[key]!r}, "
+            return __[:-2]
+        return "{" + recursive(self) + "}"
+
+    def __getitem__(self, item):
+        return self.__dict__[item]
+
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+
+    def __getattr__(self, item):
+        if item in self.__dict__.keys():
+            return self.__dict__[item]
+        else:
+            raise AttributeError(f"{item!r} is not a valid Attribute!")
---
+++ NewClass/__init__.py
@@ -0,0 +1,4 @@
+__author__ = "Albert Unruh"
+__version__ = "0.1"
+
+from .AttrDict import AttrDict
